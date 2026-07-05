a9a219d9fc528c8d277aa080e057fc0b43a526b9
fabianobassetti-dotcom <fabiano.bassetti@gmail.com>
2026-07-05 12:42:56 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+const CONFIG = {
+    PLAYLIST_URL: "https://raw.githubusercontent.com/fabianobassetti-dotcom/minha-playlist-iptv/main/ListaIPTV.m3u",
+    LOG_PREFIX: "[Bassetti TV]"
+};
